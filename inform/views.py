a9d2be1c41a978ba4840cad667a6b93932a1f63e
ChoiHyeongu <a11dsd123@gmail.com>
2018-05-27 20:08:09 +0900

    Add Message

--- inform/views.py
+++ inform/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
  
 def keyboard(request):
  
@@ -11,4 +12,17 @@ def keyboard(request):
                 'buttons' : ['급식','학사 일정']
                 })
  
-
+ def message(request):
+        message = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(message)
+        return_str = return_json_str['content']
+ 
+        return JsonResponse({
+                'message': {
+                        'text': "you type "+return_str+"!"
+                },
+                'keyboard': {
+                        'type': 'buttons',
+                        'buttons': ['급식','학사일정']
+                }
+        })
