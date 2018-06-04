f214b62f889c80d5e40f7a50e279a930be402f6a
ChoiHyeongu <a11dsd123@gmail.com>
2018-06-04 20:01:35 +0900

    Test

--- inform/views.py
+++ inform/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-import json
+from pytz import timezone
+import datetime, json  # datetime 모듈 import
+from parser import *  # parser.py import
 
 
 def keyboard(request):
@@ -12,17 +14,52 @@ def keyboard(request):
 
 
 @csrf_exempt
-def answer(request):
+def message(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
 
+    # date
+    # 오늘
+    dt1 = datetime.datetime.today()
+
+    local_date1 = dt1.strftime("%Y.%m.%d")
+    local_weekday1 = dt1.weekday()
+    # 오늘
+
+    # 내일
+    dt2 = datetime.datetime.today() + datetime.timedelta(days=1)
+
+    local_date2 = dt2.strftime("%Y.%m.%d")
+    local_weekday2 = dt2.weekday()
+    # 내일
+    # date
+
     if datacontent == '오늘':
-        today = "오늘 급식"
+
+        # 시간 관련
+        meal_date = str(local_date1)
+        l_wkday = int(local_weekday1)
+        # 시간 관련
+
+        # 파싱
+        l_l = get_diet(2, meal_date, l_wkday)
+        d_d = get_diet(3, meal_date, l_wkday)
+        # 파싱
+
+        if len(l_l) == 1:
+            lunch = "급식이 없습니다."
+            dinner = ""
+        elif len(d_d) == 1:
+            lunch = meal_date + " 중식\n" + l_l
+            dinner = ""
+        else:
+            lunch = meal_date + " 중식\n" + l_l
+            dinner = meal_date + " 석식\n" + d_d
 
         return JsonResponse({
             'message': {
-                'text': today
+                'text': lunch + dinner
             },
             'keyboard': {
                 'type': 'buttons',
@@ -31,12 +68,31 @@ def answer(request):
 
         })
 
-    elif datacontent == '내일':
-        tomorrow = "내일 급식"
+    if datacontent == '내일':
+
+        # 시간 관련
+        meal_date = str(local_date1)
+        l_wkday = int(local_weekday1)
+        # 시간 관련
+
+        # 파싱
+        l_l = get_diet(2, meal_date, l_wkday)
+        d_d = get_diet(3, meal_date, l_wkday)
+        # 파싱
+
+        if len(l_l) == 1:
+            lunch = "급식이 없습니다."
+            dinner = ""
+        elif len(d_d) == 1:
+            lunch = meal_date + " 중식\n" + l_l
+            dinner = ""
+        else:
+            lunch = meal_date + " 중식\n" + l_l
+            dinner = meal_date + " 석식\n" + d_d
 
         return JsonResponse({
             'message': {
-                'text': tomorrow
+                'text': lunch + dinner
             },
             'keyboard': {
                 'type': 'buttons',
